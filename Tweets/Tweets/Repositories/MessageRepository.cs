79923a01fd911923c919001d45b2f084299ceea6
Orehouse <orehouse@ya.ru>
2014-03-22 00:01:44 +0600

    Задание №1
    Орехов Артем

--- Tweets/Tweets/Repositories/MessageRepository.cs
+++ Tweets/Tweets/Repositories/MessageRepository.cs
@@ -1,7 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+<<<<<<< HEAD
+using System.Data.Linq;
 using System.Data.Linq.Mapping;
+using System.Data.SqlClient;
+=======
+using System.Data.Linq.Mapping;
+>>>>>>> dc96e533460e2c89708379c6b599b92bc5b340c1
 using System.Linq;
 using Tweets.ModelBuilding;
 using Tweets.Models;
@@ -23,31 +29,100 @@ namespace Tweets.Repositories
 
         public void Save(Message message)
         {
+<<<<<<< HEAD
+            //TODO: Здесь нужно реализовать вставку сообщения в базу
+            var messageDocument = messageDocumentMapper.Map(message);
+            var dataContext = new DataContext(connectionString, mappingSource);
+            var messageTable = dataContext.GetTable<MessageDocument>();
+            messageTable.InsertOnSubmit(messageDocument);
+            dataContext.SubmitChanges();
+=======
             var messageDocument = messageDocumentMapper.Map(message);
             //TODO: Здесь нужно реализовать вставку сообщения в базу
+>>>>>>> dc96e533460e2c89708379c6b599b92bc5b340c1
         }
 
         public void Like(Guid messageId, User user)
         {
+<<<<<<< HEAD
+            //TODO: Здесь нужно реализовать вставку одобрения в базу
+            var likeDocument = new LikeDocument {MessageId = messageId, UserName = user.Name, CreateDate = DateTime.UtcNow};
+            var dataContext = new DataContext(connectionString, mappingSource);
+            var likeTable = dataContext.GetTable<LikeDocument>();
+            likeTable.InsertOnSubmit(likeDocument);
+            dataContext.SubmitChanges();
+=======
             var likeDocument = new LikeDocument {MessageId = messageId, UserName = user.Name, CreateDate = DateTime.UtcNow};
             //TODO: Здесь нужно реализовать вставку одобрения в базу
+>>>>>>> dc96e533460e2c89708379c6b599b92bc5b340c1
         }
 
         public void Dislike(Guid messageId, User user)
         {
             //TODO: Здесь нужно реализовать удаление одобрения из базы
+<<<<<<< HEAD
+            var dataContext = new DataContext(connectionString, mappingSource);
+            var likeTable = dataContext.GetTable<LikeDocument>();
+            var isLiked = likeTable.FirstOrDefault(like => like.MessageId.Equals(messageId) && like.UserName.Equals(user.Name));
+            if (isLiked != null)
+            {
+                likeTable.DeleteOnSubmit(isLiked);
+                dataContext.SubmitChanges();
+            }
+=======
+>>>>>>> dc96e533460e2c89708379c6b599b92bc5b340c1
         }
 
         public IEnumerable<Message> GetPopularMessages()
         {
             //TODO: Здесь нужно возвращать 10 самых популярных сообщений
+<<<<<<< HEAD
+            var dataContext = new DataContext(connectionString, mappingSource);
+            var messageTable = dataContext.GetTable<MessageDocument>();
+            var likesTable = dataContext.GetTable<LikeDocument>();
+            var messages = messageTable
+                .Select(message => new Message()
+                {
+                    CreateDate = message.CreateDate,
+                    Id = message.Id,
+                    Text = message.Text,
+                    User = new User() { Name = message.UserName},
+                    Likes = likesTable.Count(like => like.MessageId.Equals(message.Id))
+                })
+                .Take(10)
+                .OrderByDescending(message => message.Likes)
+                .AsEnumerable()
+                .ToArray();
+            return messages;
+=======
             return Enumerable.Empty<Message>();
+>>>>>>> dc96e533460e2c89708379c6b599b92bc5b340c1
         }
 
         public IEnumerable<UserMessage> GetMessages(User user)
         {
             //TODO: Здесь нужно получать все сообщения конкретного пользователя
+<<<<<<< HEAD
+            var dataContext = new DataContext(connectionString, mappingSource);
+            var messageTable = dataContext.GetTable<MessageDocument>();
+            var likesTable = dataContext.GetTable<LikeDocument>();
+            var messages = messageTable
+                .Where(message => message.UserName.Equals(user.Name))
+                .Select(message => new UserMessage()
+                {
+                    CreateDate = message.CreateDate,
+                    Id = message.Id,
+                    Text = message.Text,
+                    User = user,
+                    Likes = likesTable.Count(like => like.MessageId.Equals(message.Id)),
+                    Liked = likesTable.Count(like => like.MessageId.Equals(message.Id) && like.UserName.Equals(message.UserName)) != 0
+                })
+                .OrderByDescending(message => message.CreateDate)
+                .AsEnumerable();
+            return messages;
+=======
             return Enumerable.Empty<UserMessage>();
+>>>>>>> dc96e533460e2c89708379c6b599b92bc5b340c1
         }
     }
 }
---
+++ Tweets/dbCreate.sql
@@ -0,0 +1,19 @@
+﻿DROP	TABLE [dbo].[messages]
+CREATE TABLE [dbo].[messages] (
+    [Id]         UNIQUEIDENTIFIER NOT NULL,
+    [userName]   VARCHAR (100)    NULL,
+    [text]       VARCHAR (1000)   NULL,
+    [createDate] DATETIME         NULL,
+    [version]    ROWVERSION       NOT NULL,
+	CONSTRAINT PK_Tmp1 PRIMARY KEY ([Id]),
+);
+
+DROP TABLE [dbo].[likes]
+CREATE TABLE [dbo].[likes] (
+    [userName]   VARCHAR (100)    NOT NULL,
+    [messageId]  UNIQUEIDENTIFIER NOT NULL,
+    [createDate] DATETIME         NULL,
+	CONSTRAINT PK_Tmp PRIMARY KEY ([userName], [messageId]),
+	CONSTRAINT FK_Tmp2 FOREIGN KEY ([messageId]) REFERENCES [dbo].[messages] (Id)
+)
+select * from [dbo].messages
